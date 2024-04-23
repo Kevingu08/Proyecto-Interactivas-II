a8eedc9c97b9aa286228601c677fc11c7e6b36e8
Kevingu08 <guidourbinakevin@gmail.com>
2024-04-23 11:04:11 -0600

    tailwind configuration update, variables added

--- Jint/tailwind.config.js
+++ Jint/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors:{
+            'primary': '#966BDD',
+            'dark': '#1D1D1D',
+            'dark-secondary': '#353535',
+            'gray': '#8A8A8A',
+        },
+        fontFamily: {
+            'primary': ['Montserrat', 'sans-serif'],
+        },
+        backgroundImage: {
+            'gradient-light': 'linear-gradient(270deg, rgba(226,215,246,1) 0%, rgba(185,156,232,1) 100%)',
+            'gradient-dark': 'linear-gradient(270deg, rgba(69,41,117,1) 0%, rgba(114,70,187,1) 100%)',
+        },
+        boxShadow:{
+            'main': '10px 5px 30px 0px rgba(0,0,0,0.20)',
+        }
+    },
   },
   plugins: [],
 }
