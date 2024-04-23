f769651633863f4bfb31a2e300bbe138918d3de6
@Emanuel260902 <emanuel.aguero@ucr.ac.cr>
2024-04-23 15:16:02 -0600

    Merge branch 'main' of https://github.com/Kevingu08/Proyecto-Interactivas-II

--- Jint/src/components/App.jsx
+++ Jint/src/components/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
-import { Sidebar } from './Daily Weekly Resume/Sidebar.jsx'
+import { Sidebar } from './Sidebar.jsx'
 
 function App() {
 
