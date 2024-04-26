69f1d4e1aa171f2f20c0a779192b5f0fcd6d6244
LeoUlate <leoulateh@gmai.com>
2024-04-25 18:57:44 -0600

    Historic tasks update

--- Jint/src/components/App.jsx
+++ Jint/src/components/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './index.css'
 import { Sidebar } from './Sidebar.jsx'
 import { Tasks } from './Tasks.jsx'
+import { Historic } from './Historic Resume/History.jsx'
 
 export function App() {
   const task = [
@@ -14,7 +15,7 @@ export function App() {
   return (
     <>
       <Sidebar/>
-      <Tasks/>
+      <Historic/>
     </>
   )
 }
