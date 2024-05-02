a0e8257ca66fb276bba1411fb63319e2ae5810b7
ChillyGuro <vasquezc416gmail.com>
2024-05-02 01:54:28 -0600

    Added some responsive functions to AdminView

--- Jint/src/components/AdminView.jsx
+++ Jint/src/components/AdminView.jsx
@@ -4,7 +4,7 @@ import './Sidebar'
 export function AdminView() {
     return (
         <div className='h-screen bg-white m-auto border'>
-            <div className='container  rounded-2xl max-w-[600px] h-auto shadow-lg flex flex-col items-center justify-center pt-14'>
+            <div className='container m-6 rounded-2xl max-w-[600px] h-auto shadow-2xl flex flex-col items-center justify-center pt-6 p-auto'>
                 <h1 className='font-semibold text-balck text-xl'>ASSIGN NEW TASK</h1>
                 <div>
                     <div class="container m-3 grid grid-cols-2 gap-10 mt-16">
@@ -22,8 +22,8 @@ export function AdminView() {
                         </div>
                     </div>
                 </div>
-                <div className='container rounded-2xl w-[500px] h-[200px] shadow-2xl text-justify p-4 m-16'>
-                    <h2 className=''>Add a description:</h2>
+                <div className='container rounded-2xl max-w-[500px] min-w-2 max-h-96 min-h-[200px] shadow-2xl text-justify p-4 m-16'>
+                    <h2 className='text-sm font-regular text-gray'>Add a description:</h2>
                 </div>
             </div>
         </div>
