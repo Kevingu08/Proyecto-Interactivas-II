caffbbe2c95ee7d281063bd4b2acdca5fba7e6e8
ChillyGuro <vasquezc416gmail.com>
2024-05-08 16:43:03 -0600

    Design changes in adminview

--- Jint/src/components/AdminView.jsx
+++ Jint/src/components/AdminView.jsx
@@ -7,41 +7,16 @@ import { Sidebar } from './Sidebar';
 export function AdminView() {
     return (
         <>
-        <div>
-        <Sidebar />
-        </div>
-            <div className='h-screen flex flex-row justify-center items-center gap-9 '>
-                <div className='container rounded-2xl m-12 w-[910px] text-justify '>
-                    <Calendar />
-                </div>
-                <div className='container m-6 rounded-2xl max-w-[600px] h-[1000px] shadow-2xl flex flex-col items-center justify-center pt-6 p-auto '>
-                    <h1 className='font-semibold text-balck text-4xl'>ASSIGN NEW TASK</h1>
-                    <div>
-                        <div class="container m-3 grid grid-cols-2 gap-8 mt-16">
-                            <div className='flex flex-col items-center justify-center'>
-                                <h2 class="text-3xl font-bold">Specifications</h2>
-                                <div className='flex flex-col justify-center items-center font- text-3xl gap-5 mt-3'>
-                                <Dropdown name="Group" options={['Option 1', 'Option 2', 'Option 3']} fontSize="text-xl"/>
-                                <Dropdown name="Course" options={['Option 1', 'Option 2', 'Option 3']} fontSize="text-xl"/>
-                                <Dropdown name="Filter" options={['Option 1', 'Option 2', 'Option 3']} fontSize="text-xl"/>
-                                </div>
-                            </div>
-                            <div className='flex flex-col items-center  justify-center'>
-                                <h2 class="text-3xl font-bold">Date</h2>
-                                <div className='container m-auto flex flex-col justify-center  items-center font- text-3xl gap-5 mt-3'>
-                                <Dropdown name="Hour" options={['Option 1', 'Option 2', 'Option 3','5']} fontSize="text-xl"/>
-                                <Dropdown name="Day" options={['Option 1', 'Option 2', 'Option 3']} fontSize="text-xl"/>
-                                <Dropdown name="Month" options={['Option 1', 'Option 2', 'Option 3']} fontSize="text-xl"/>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div className='container rounded-2xl max-w-[500px] min-w-2 max-h-96 min-h-[300px] shadow-2xl text-justify p-4 m-12'>
-                        <h2 className='text-sm font-regular text-gray'>Add a description:</h2>
-                    </div>
-                </div>
-            </div>
+
+<div class="h-screen grid grid-cols-12 gap-52">
+  <div class="col-start-1 col-end-2 border border-gray-400">
+    <Sidebar/>
+  </div>
+  <div class="col-span-5 border border-gray-400">Columna 2</div>
+  <div class="col-span-6 item-center border border-gray-400 p-12 ">
+  <Calendar/>
+  </div>
+</div>
         </>
     )
 }
--- Jint/src/components/Login.jsx
+++ Jint/src/components/Login.jsx
@@ -10,18 +10,18 @@ export function Login() {
                 <form className="mt-4 flex flex-col gap-6 w-full mb-10">
                     <div className="shadow-main p-2 rounded-xl bg-white flex gap-2">
                         <svg className="w-6 h-6 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                          <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd" />
                         </svg>
-                        <input className='w-[90%]' type="text"  placeholder="User" />
+                        <input className='w-[90%]' type="text" placeholder="User" />
                     </div>
                     <div className="shadow-main p-2 rounded-xl bg-white flex gap-2">
                         <svg className="w-6 h-6 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                          <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clip-rule="evenodd" />
                         </svg>
                         <input className='w-[90%]' type="password" placeholder="Password" />
                     </div>
 
-                    <input className='text-white bg-white/30 rounded-xl w-fit m-auto px-8 py-2 cursor-pointer hover:bg-white hover:text-primary hover:font-semibold' type="submit" value="Login"/>
+                    <input className='text-white bg-white/30 rounded-xl w-fit m-auto px-8 py-2 cursor-pointer hover:bg-white hover:text-primary hover:font-semibold' type="submit" value="Login" />
                 </form>
                 <p className='text-white'>Don’t you have an account? <a className='font-bold underline decoration-2' href="#">REGISTER</a></p>
             </div>
