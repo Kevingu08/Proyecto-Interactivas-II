51620308bfae115b4e2581a08706c13912d90fb8
ChillyGuro <vasquezc416gmail.com>
2024-05-09 16:59:30 -0600

    Added responsive to admin view

--- Jint/src/components/AdminView.jsx
+++ Jint/src/components/AdminView.jsx
@@ -3,48 +3,47 @@ import { Dropdown } from './Dropdown';
 import svgImage from "../assets/imgs/elipse.svg";
 import './index.css'
 
+//responsive prueba
+
 export function AdminView() {
     return (
         <>
-            <section className=' grid grid-cols-2 sm:grid-cols-2  xl:ml-[15rem] gap-2  dark:bg-dark-primary dark:text-white'>
-                <div className='m-12'>
-                    <h1 className='text-4xl font-medium'>ASSAIGN NEW TASK</h1>
-                    <div >
-                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mr-80 mb-4 '>Description</h2>
-                        <div className='grid grid-cols-2 gap-4 mt-4'>
+            <section className='grid xl:grid-cols-2 xl:ml-[15rem] sm:p-10 sm:grid-cols-1  gap-2 dark:bg-dark-primary dark:text-white'>
+                <div className='m-4 sm:m-12'>
+                    <h1 className='text-4xl font-medium'>ASSIGN NEW TASK</h1>
+                    <div>
+                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mb-4'>Description</h2>
+                        <div className='grid grid-cols-1 sm:grid-cols-1 gap-4 mt-4'>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Title</h1>
-                                <input type="text" class="w-80 py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md  dark:bg-white" placeholder="Escribe aquí..." />
+                                <input type="text" className="w-full py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white" placeholder="Escribe aquí..." />
                             </div>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Description</h1>
-                                <input type="text" class="w-80 py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md  dark:bg-white" placeholder="Escribe aquí..." />
-                            </div>
-                            <div>
-
+                                <input type="text" className="w-full py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white" placeholder="Escribe aquí..." />
                             </div>
                         </div>
                     </div>
                     <div>
-                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mr-80 mb-4 '>Specifications</h2>
-                        <div className='grid grid-cols-2 gap-4 mt-4'>
+                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mb-4'>Specifications</h2>
+                        <div className='grid grid-cols-1 sm:grid-cols-1 gap-4 mt-4'>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Group</h1>
-                                <input type="text" class="w-80 py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white " placeholder="Escribe aquí..." />
+                                <input type="text" className="w-full py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white" placeholder="Escribe aquí..." />
                             </div>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Course</h1>
-                                <input type="text" class="w-80 py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md  dark:bg-white" placeholder="Escribe aquí..." />
+                                <input type="text" className="w-full py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white" placeholder="Escribe aquí..." />
                             </div>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Filter</h1>
-                                <input type="text" class="w-80 py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md  dark:bg-white" placeholder="Escribe aquí..." />
+                                <input type="text" className="w-full py-3 px-3 border bg-input-bg bg-opacity-50 border-black border-opacity-25 rounded-md dark:bg-white" placeholder="Escribe aquí..." />
                             </div>
                         </div>
                     </div>
                     <div className='pb-4'>
-                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mr-80 mb-4 '>Date</h2>
-                        <div className='grid grid-cols-2 gap-4 mt-4'>
+                        <h2 className='text-xl font-bold mt-10 border-b border-gray border-opacity-90 pb-3 mb-4'>Date</h2>
+                        <div className='grid grid-cols-1 sm:grid-cols-1 gap-4 mt-4'>
                             <div>
                                 <h1 className='text-xl font-medium m-4'>Day</h1>
                                 <div>
@@ -66,7 +65,7 @@ export function AdminView() {
                         </div>
                     </div>
                 </div>
-                <div className='p-24'>
+                <div className='p-4 sm:p-24'>
                     <Calendar />
                 </div>
 
