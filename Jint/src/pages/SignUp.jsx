6e14434df9980a57ea425cf91403e39fc3febe38
Carlos Vasquez <115187830+ChillyGuaro@users.noreply.github.com>
2024-06-21 14:17:58 -0600

    SingUp changes

--- Jint/src/pages/SignUp.jsx
+++ Jint/src/pages/SignUp.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import { ROUTE_PATHS } from '../routes';
 import { NavLink } from 'react-router-dom';
 import { Input } from '../components/Input';
 import { UserIcon } from '../components/Icons/UserICon';
 import { EmailIcon } from '../components/Icons/EmailIcon';
 import { LockIcon } from '../components/Icons/LockIcon';
 
+
 export function SignUp() {
-    const [user, setUser] = useState('');
+    const [username, setUser] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [lastname, setLastName] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleSignUp = async (e) => {
@@ -20,18 +22,16 @@ export function SignUp() {
             return;
         }
 
-        const profile_type_id = 1; // Adjust this value based on your application's logic
+        const profile_type_id = 1; 
 
         try {
             const response = await fetch('http://jint_backend.test/api/users/store', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
                 body: JSON.stringify({
+                    username,
                     name,
                     email,
-                    lastname: lastName,
+                    lastname,
                     password,
                     profile_type_id,
                 }),
@@ -57,7 +57,7 @@ export function SignUp() {
                 <p className="text-center text-white">
                     Welcome! Come on and create your account{' '}
                 </p>
-                <form className="mt-4 flex flex-col gap-6 w-full mb-10" onSubmit={handleSignUp}>
+                <form method='POST' className="mt-4 flex flex-col gap-6 w-full mb-10" onSubmit={handleSignUp}>
                     <Input
                         title={'Name'}
                         onChange={(e) => setName(e.target.value)}
@@ -68,14 +68,14 @@ export function SignUp() {
                     <Input
                         title={'Last Name'}
                         onChange={(e) => setLastName(e.target.value)}
-                        inputId={'lastName'}
+                        inputId={'lastname'}
                         icon={<UserIcon />}
                         type={'text'}
                     />
                     <Input
                         title={'User'}
                         onChange={(e) => setUser(e.target.value)}
-                        inputId={'user'}
+                        inputId={'username'}
                         icon={<UserIcon />}
                         type={'text'}
                     />
@@ -120,3 +120,65 @@ export function SignUp() {
     );
 }
 
+/* import { NavLink } from 'react-router-dom'
+import { ROUTE_PATHS } from '../routes'
+import { Input } from '../components/Input'
+import { UserIcon } from '../components/Icons/UserICon'
+import { EmailIcon } from '../components/Icons/EmailIcon'
+import { LockIcon } from '../components/Icons/LockIcon'
+
+export function SignUp() {
+    return (
+        <section className="flex flex-col justify-center items-center h-screen bg-primary">
+            <div className="flex flex-col items-center justify-center w-80">
+                <h2 className="font-bold text-3xl text-center text-white mb-2">
+                    Sing Up
+                </h2>
+                <p className="text-center text-white">
+                    Welcome! Come on and create your account{' '}
+                </p>
+
+                <form className="mt-4 flex flex-col gap-6 w-full mb-10">
+                    <Input
+                        title={'User'}
+                        inputId={'userInput'}
+                        icon={<UserIcon />}
+                        type={'text'}
+                    />
+                    <Input
+                        title={'Email'}
+                        inputId={'emailInput'}
+                        icon={<EmailIcon />}
+                        type={'email'}
+                    />
+                    <Input
+                        title={'Password'}
+                        inputId={'passwordInput'}
+                        icon={<LockIcon />}
+                        type={'password'}
+                    />
+                    <Input
+                        title={'Confirm Password'}
+                        inputId={'passwordConfirmInput'}
+                        icon={<LockIcon />}
+                        type={'password'}
+                    />
+                    <input
+                        className="w-full text-white bg-dark font-semibold rounded-xl px-8 py-2 cursor-pointer border-2 mt-2 border-dark hover:bg-white hover:text-dark transition-colors duration-200"
+                        type="submit"
+                        value="Sing Up"
+                    />
+                </form>
+                <p className="text-white">
+                    Do you have an account?{' '}
+                    <NavLink
+                        className="font-bold underline decoration-2"
+                        to={ROUTE_PATHS.LOGIN}
+                    >
+                        LOGIN
+                    </NavLink>
+                </p>
+            </div>
+        </section>
+    )
+} */
